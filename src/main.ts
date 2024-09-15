4fdbc6b95e29464560a4d80b822fe33cdb4cfd0d
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2024-09-16 00:32:07 +0200

    ts update

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@
 // const list = document.querySelector('#recipeList');
 const dataUrl = "recipes.json";
 
-// const recipeTemplate = (recipe: any) => {
-//   const { name, preparation } = recipe;
+const recipeTemplate = (recipe: any) => {
+  const { name, preparation } = recipe;
 
-//   return `<div class="recipe">
-//     ${(name) && `<div class="recipe__name">${name}</div>`}
-//     ${preparation && `<div class="recipe__preparation">${preparation}</div>`}
-//     </div>`
-// };
+  return `<div class="recipe">
+    ${(name) && `<div class="recipe__name">${name}</div>`}
+    ${preparation && `<div class="recipe__preparation">${preparation}</div>`}
+    </div>`
+};
 
 async function init() {
   console.log('lang', navigator.language);
@@ -22,11 +22,11 @@ async function init() {
 
   console.log('lang', res, json);
 
-  // for (const kye in json) {
-  //   if (list && json[kye]) {
-  //     list.innerHTML += recipeTemplate(json[kye]);
-  //   }
-  // }
+  for (const kye in json) {
+    if (list && json[kye]) {
+      list.innerHTML += recipeTemplate(json[kye]);
+    }
+  }
 };
 
 init();
