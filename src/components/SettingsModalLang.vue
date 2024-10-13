ff300015c5d978d3d16aa6c6d828e0d413643534
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2024-10-13 14:39:34 +0200

    template moved above script

--- src/components/SettingsModalLang.vue
+++ src/components/SettingsModalLang.vue
@@ -1,3 +1,21 @@
+<template>
+    <section class="lang">
+        <span>Language</span>
+
+        <div class="customSelect">
+            <select @change="change($event)">
+                <option value="auto">auto</option>
+                <option v-for="lang of langList" :value="lang">{{ lang }}</option>
+            </select>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+                <path
+                    d="M459-381 314-526q-3-3-4.5-6.5T308-540q0-8 5.5-14t14.5-6h304q9 0 14.5 6t5.5 14q0 2-6 14L501-381q-5 5-10 7t-11 2q-6 0-11-2t-10-7Z" />
+            </svg>
+        </div>
+
+    </section>
+</template>
+
 <script lang="ts">
 export default {
     data() {
@@ -55,24 +73,6 @@ export default {
     }
 }
 </script>
-
-<template>
-    <section class="lang">
-        <span>Language</span>
-
-        <div class="customSelect">
-            <select @change="change($event)">
-                <option value="auto">auto</option>
-                <option v-for="lang of langList" :value="lang">{{ lang }}</option>
-            </select>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
-                <path
-                    d="M459-381 314-526q-3-3-4.5-6.5T308-540q0-8 5.5-14t14.5-6h304q9 0 14.5 6t5.5 14q0 2-6 14L501-381q-5 5-10 7t-11 2q-6 0-11-2t-10-7Z" />
-            </svg>
-        </div>
-
-    </section>
-</template>
 
 <style scoped>
 .lang {
