b5bba97cf745ec5389a44be28603edb9bbb32cdf
Lukasz Dzielo <lukasz.dzielo@gmail.com>
2024-10-15 15:43:11 +0200

    footer update

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,25 @@
 <template>
     <footer>
         <div class="container">
-            With 💙 to ☕ by LD </div>
+            <span>Made by</span>
+            <a href="https://lukaszdzielo.github.io/" target="_blank" class="link">
+                <svg width="24" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 200 200">
+                    <defs>
+                        <linearGradient id="a" x1="201.42" y1="229.25" x2="351.8" y2="379.62"
+                            gradientUnits="userSpaceOnUse">
+                            <stop offset="0" stop-color="var(--gray-600)"></stop>
+                            <stop offset="1" stop-color="var(--gray-600)"></stop>
+                        </linearGradient>
+                    </defs>
+                    <path
+                        d="M2e2 295v95h95a95 95 0 000-190H237.74v85.35l37.75-10.08v38.6L237.74 324v28.31h56.19a57.26 57.26.0 100-114.52H275.49"
+                        transform="translate(-195 -195)"
+                        style="fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:10px;stroke:url(#a)">
+                    </path>
+                </svg>
+            </a>
+        </div>
     </footer>
 </template>
 
@@ -13,5 +31,16 @@ footer {
     color: var(--gray-600);
     font-size: 0.75rem;
     text-align: center;
+}
+
+.container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: .5rem;
+}
+
+.link {
+    display: flex;
 }
 </style>
